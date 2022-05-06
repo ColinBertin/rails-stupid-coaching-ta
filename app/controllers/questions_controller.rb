56dd6e3c88e3db6ff86a66cea67689854e61fa61
Colin Bertin <colinbertin12@gmail.com>
2022-05-06 15:12:34 +0900

    testing test case

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,17 +2,15 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    @question = params[:request]
-    @answer = if @question == @question.upcase && @question != "I AM GOING TO WORK RIGHT NOW!"
-      'I can feel your motivation!'
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
     else
-      if @question.include?('?')
-        'Silly question, get dressed and go to work!'
-      elsif @question.downcase == 'i am going to work right now!'
-        ''
-      else
-        "I don't care, get dressed and go to work!"
-      end
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
